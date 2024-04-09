b5cc4c5aa3533ac5330f75865be4bc2e18e603af
Breno1221 <123486496+Breno121zs@users.noreply.github.com>
2024-04-09 10:31:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const name = 'John';
+const age = 30;
+
+const person = {
+    name: 'Brad',
+    age: 40,
+};
+
+let newName = name;
+newName = 'Jhonantan';
+
+
+let newPerson = person;
+newPerson.name = 'Breadley'
+
+
+console.log(name, newName); 
+console.log(person, newPerson);
